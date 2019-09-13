16d90a9ecacb735336e259edebc231e068d2a140
ale <alexandraandrade636@gmail.com>
2019-09-12 21:57:19 -0300

    aluno1

---
+++ EscoladaAle/src/Aluno.java
@@ -0,0 +1,8 @@
+
+public class Aluno {  
+	String nome;
+	int idade;
+	double nota1;
+	double nota2;
+
+}
---
+++ EscoladaAle/src/CriarAluno.java
@@ -0,0 +1,13 @@
+
+public class CriarAluno {
+
+	public static void main(String[] args) {
+		Aluno aluno1= new Aluno();
+		aluno1.nome = "Juliana";
+		aluno1.idade = 48;
+		aluno1.nota1 = 8;
+		aluno1.nota2= 10;
+
+	}
+
+}
